3fe21d98d7ab8bdedb052c00866d5cad2b9803d3
Szegedi Attila <szegedi.attila@virgo.hu>
2015-12-10 14:01:25 +0100

    indent fix

--- public/js/client.js
+++ public/js/client.js
@@ -1,51 +1,51 @@
 !function(doc, win){
 
-	function getTime(timeStamp){
-		var date = new Date(+timeStamp),
-			options = {
-				hour: "2-digit",
-				minute: "2-digit",
-				second: "2-digit"
-			};
+    function getTime(timeStamp){
+        var date = new Date(+timeStamp),
+            options = {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit"
+            };
 
-		return date.toLocaleString('hu-HU', options);
-	}
+        return date.toLocaleString('hu-HU', options);
+    }
 
-	function createMessage(type, e){
-		if(arguments.length === 1) {
-			var e = arguments[0],
-				type = null;
-		}
+    function createMessage(type, e){
+        if(arguments.length === 1) {
+            var e = arguments[0],
+                type = null;
+        }
 
-		var $list 	  = $('#messages'),
-			time      = getTime(+e.lastEventId),
-			message   = e.data,
-			className = !!type ? 'alert alert-' + type : '';
+        var $list     = $('#messages'),
+            time      = getTime(+e.lastEventId),
+            message   = e.data,
+            className = !!type ? 'alert alert-' + type : '';
 
-		$list.append('<p class="'+className+'"><time>'+time+'</time> '+message+'</p>');
-	}
+        $list.append('<p class="'+className+'"><time>'+time+'</time> '+message+'</p>');
+    }
 
-	var source = new EventSource('/stream');
+    var source = new EventSource('/stream');
 
-	source.addEventListener('message', $.proxy(createMessage, this));
-	source.addEventListener('connection', $.proxy(createMessage, this, 'info'));
-	source.addEventListener('disconnection', $.proxy(createMessage, this, 'danger'));
+    source.addEventListener('message', $.proxy(createMessage, this));
+    source.addEventListener('connection', $.proxy(createMessage, this, 'info'));
+    source.addEventListener('disconnection', $.proxy(createMessage, this, 'danger'));
 
-	$('#chat-form').on('submit', function(e){
-		var $form  = $(this),
-			$input = $('input', $form),
-			data   = $form.serializeArray();
+    $('#chat-form').on('submit', function(e){
+        var $form  = $(this),
+            $input = $('input', $form),
+            data   = $form.serializeArray();
 
-		$.ajax({
-			url: '/chat',
-			data: jQuery.param(data),
-			method: 'post',
-			success: function(e){
-				$input.val('');
-			}
-		});
+        $.ajax({
+            url: '/chat',
+            data: jQuery.param(data),
+            method: 'post',
+            success: function(e){
+                $input.val('');
+            }
+        });
 
-		return false;
-	});
+        return false;
+    });
 
 }(document, window);
--- server.js
+++ server.js
@@ -10,60 +10,60 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(serve('./public'));
 
 function sendMessages(message, type){
-	// lerakjuk a datumot
-	var date = +new Date();
+    // lerakjuk a datumot
+    var date = +new Date();
 
-	// vegig megyunk az osszes memoriaban eltarolt kliensen
-	_.each(clients, function(client){
-		var data = 'id: ' + date + '\n';
+    // vegig megyunk az osszes memoriaban eltarolt kliensen
+    _.each(clients, function(client){
+        var data = 'id: ' + date + '\n';
 
-		// connection eventet kuldunk ha csatlakoztak
-		if(type === 'connected') {
-			data += 'event: connection\n';
-		}
-		// disconnection eventet kuldunk ha lecsatlakoztak
-		else if(type === 'disconnected'){
-			data += 'event: disconnection\n';
-		}
+        // connection eventet kuldunk ha csatlakoztak
+        if(type === 'connected') {
+            data += 'event: connection\n';
+        }
+        // disconnection eventet kuldunk ha lecsatlakoztak
+        else if(type === 'disconnected'){
+            data += 'event: disconnection\n';
+        }
 
-		// hozzaadjuk az uzenetet
-		data += 'data: ' + message + '\n\n';
+        // hozzaadjuk az uzenetet
+        data += 'data: ' + message + '\n\n';
 
-		// elkuldjuk a data-t
-		client.write(data);
-	});
+        // elkuldjuk a data-t
+        client.write(data);
+    });
 }
 
 // stream vegponton figyelunk a csatlakozasokra
 app.get('/stream', function(req, res, next){
 
-	// beallitjuk a header ertekeket
-	res.writeHead(200, {
-		"Content-Type": "text/event-stream",
-		"Cache-Control": "no-cache",
-		"Connection": "keep-alive"
-	});
+    // beallitjuk a header ertekeket
+    res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive"
+    });
 
-	// a mar csatlakozott klienseknek kikuldunk egy infot
-	sendMessages('client connected from ' + req.ip, 'connected');
+    // a mar csatlakozott klienseknek kikuldunk egy infot
+    sendMessages('client connected from ' + req.ip, 'connected');
 
-	// elrakjuk memoriaba az uj klienst
-	clients.push(res);
+    // elrakjuk memoriaba az uj klienst
+    clients.push(res);
 
-	// ha lezarul a kapcsolat (megszakad), kikuldunk egy warningot a klienseknek
-	req.on("close", function() {
-    	sendMessages('client disconnected from' + req.ip, 'disconnected')
-  	});
+    // ha lezarul a kapcsolat (megszakad), kikuldunk egy warningot a klienseknek
+    req.on("close", function() {
+        sendMessages('client disconnected from' + req.ip, 'disconnected')
+    });
 });
 
 // chat vegponton varjuk az uzeneteket
 app.post('/chat', function(req, res, next){
 
-	// elkuldjuk a kliens ip-jet es a messaget
-	sendMessages(req.ip + '   ' + req.body.message);
-	
-	// 200 OK-val visszaterunk
-	res.status(200).end();
+    // elkuldjuk a kliens ip-jet es a messaget
+    sendMessages(req.ip + '   ' + req.body.message);
+    
+    // 200 OK-val visszaterunk
+    res.status(200).end();
 });
 
 // 3000-es porton inditjuk a szervert
